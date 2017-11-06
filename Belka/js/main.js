e1f23db5b0061db50565e0de9048fa687b962d17
Thegek <thegek25@gmail.com>
2017-11-06 05:32:22 +0300

    Стилизация Трасс 320 + слайдер.

--- Belka/js/main.js
+++ Belka/js/main.js
@@ -10,6 +10,51 @@ $(function(){
 
     });
 
+    $('#trails__slider').slick({
+    mobileFirst: true,
+    slidesToShow: 1,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 1280,
+      settings: "unslick"
+    }
+    ]
+    
+  });
+
+
+  $('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
 });
 
 
